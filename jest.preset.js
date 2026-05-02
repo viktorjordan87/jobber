1d8c80ad3f65685938607db846d1165e57107b3a
Viktor Jordan <jordangergoviktor@gmail.com>
2026-05-02 22:49:14 +0200

    Github Workflow

--- jest.preset.js
+++ jest.preset.js
@@ -1,3 +1,8 @@
 const nxPreset = require('@nx/jest/preset').default;
 
-module.exports = { ...nxPreset, passWithNoTests: true };
+module.exports = {
+  ...nxPreset,
+  passWithNoTests: true,
+  // uuid@14 ships ESM under dist-node; allow ts-jest to transform it in CJS test runs
+  transformIgnorePatterns: [String.raw`node_modules/(?!uuid/)`],
+};
